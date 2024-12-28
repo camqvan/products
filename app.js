06227bbd967cb19e03d0abfd7e294b3a7603ae5b
Cam Van <camqvan@gmail.comls>
2024-12-28 11:00:30 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,40 @@
+const url = 'https://www.course-api.com/javascript-store-products';
+
+const productsDOM = document.querySelector('.products-center');
+
+const fetchProducts = async () => {
+  productsDOM.innerHTML = '<div class="loading"></div>';
+  try {
+    const resp = await fetch(url);
+    const data = await resp.json();
+    return data;
+  } catch (error) {
+    productsDOM.innerHTML = '<div class="error">there was an error</div>';
+  }
+};
+
+const displayProducts = list => {
+  const productList = list
+    .map(product => {
+      const { id } = product;
+      const { name: title, price } = product.fields;
+      const { url: img } = product.fields.image[0];
+      const formatPrice = price / 100;
+      return `<a class="single-product" href="product.html?id=${id}&name=john&age=25">
+            <img src="${img}" class="single-product-img img" alt="${title}" />
+            <footer>
+              <h5 class="name">${title}</h5>
+              <span class="price">${formatPrice}</span>
+            </footer>
+          </a>`;
+    })
+    .join('');
+  productsDOM.innerHTML = `<div class="products-container">${productList}</div>`;
+};
+
+const start = async () => {
+  const data = await fetchProducts();
+  displayProducts(data);
+};
+
+start();
